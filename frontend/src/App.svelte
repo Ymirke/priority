5a171d3f64615915d4ca2e218de448be1cad26e9
Ymir Egilson <ymir.egilson@gmail.com>
2020-12-01 10:30:49 +0100

    Removed scrollbar for aesthetics and improved drop behaviour

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -8,8 +8,6 @@
     --dark: rgb(19, 20, 23);
     --gray: rgb(84, 87, 100);
     --select: rgb(24, 25, 28);
-    --maxWidth: calc(100% - --sidepadding * 2);
-    --maxHeight: calc(100vh);
     margin: 0;
     background-color: var(--dark);
     color: var(--gray);
--- frontend/src/pages/Dashboard.svelte
+++ frontend/src/pages/Dashboard.svelte
@@ -2,7 +2,7 @@
   import { flip } from 'svelte/animate';
   import { dndzone } from 'svelte-dnd-action';
 
-  const flipDurationMs = 175;
+  const flipDurationMs = 100;
 
   let data = [
     {
@@ -13,36 +13,40 @@
         { id: 2, name: 'item2' },
         { id: 3, name: 'item3' },
         { id: 4, name: 'item4' },
+        { id: 5, name: 'item5' },
       ],
     },
     {
       id: 2,
       name: 'This week',
       items: [
-        { id: 5, name: 'item5' },
         { id: 6, name: 'item6' },
         { id: 7, name: 'item7' },
         { id: 8, name: 'item8' },
+        { id: 9, name: 'item9' },
+        { id: 10, name: 'item10' },
       ],
     },
     {
       id: 3,
       name: 'Today',
       items: [
-        { id: 9, name: 'item9' },
-        { id: 10, name: 'item10' },
         { id: 11, name: 'item11' },
         { id: 12, name: 'item12' },
+        { id: 13, name: 'item13' },
+        { id: 14, name: 'item14' },
+        { id: 15, name: 'item15' },
       ],
     },
     {
       id: 4,
       name: 'Done',
       items: [
-        { id: 13, name: 'item13' },
-        { id: 14, name: 'item14' },
-        { id: 15, name: 'item15' },
         { id: 16, name: 'item16' },
+        { id: 17, name: 'item17' },
+        { id: 18, name: 'item18' },
+        { id: 19, name: 'item19' },
+        { id: 20, name: 'item20' },
       ],
     },
   ];
@@ -62,30 +66,36 @@
 
 <style>
   main {
-    width: var(--maxWidth);
-    height: var(--maxHeight);
-
+    height: 100vh;
+    width: 100%;
     display: flex;
     justify-content: center;
-    width: 100%;
   }
   .column {
-    height: 90vh;
+    height: auto;
     width: 250px;
-    padding: 0.5em;
-    margin: 1em;
+    padding: 7px;
+    margin: 15px;
     float: left;
-    border: 1px solid #333333;
     overflow-y: hidden;
   }
-  .column-title {
+  .column__title {
     margin-bottom: 1em;
     display: flex;
     justify-content: center;
     align-items: center;
+    padding-top: 5px;
+    padding-bottom: 5px;
   }
-  .column-content {
-    height: 100%;
+  .column__content {
+    height: 90%;
+    overflow-y: scroll;
+    /* Hide scroll bar */
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+  .column__content::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera */
   }
   .task {
     color: white;
@@ -109,9 +119,9 @@
 <main>
   {#each data as column (column.id)}
     <div class="column">
-      <div class="column-title">{column.name}</div>
+      <div class="column__title">{column.name}</div>
       <div
-        class="column-content"
+        class="column__content"
         use:dndzone={{ items: column.items, flipDurationMs, dropTargetStyle: { outline: 'none' } }}
         on:consider={(event) => consider(event, column.id)}
         on:finalize={(event) => finalize(event, column.id)}>
